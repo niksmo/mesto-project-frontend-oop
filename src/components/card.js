992620327e2b8636225fbe72d90afe5c67f4874b
Stanislav Levchenko <staslevchenko@protonmail.com>
2022-05-18 22:09:33 +0300

    started creating card element

--- src/components/card.js
+++ src/components/card.js
@@ -4,99 +4,157 @@ import { deleteLike, putLike } from "./api";
 
 export const removebleCard = {} //глобальная переменная для удаления карточки
 
-const galleryItemTemplate = document.querySelector('#gallery__item').content;
-const popupViewPhoto = document.querySelector('.popup_feature_photo');
-const photo = document.querySelector('.view-photo__image');
-const caption = document.querySelector('.view-photo__caption');
+// const galleryItemTemplate = document.querySelector('#gallery__item').content;
+// const popupViewPhoto = document.querySelector('.popup_feature_photo');
+// const photo = document.querySelector('.view-photo__image');
+// const caption = document.querySelector('.view-photo__caption');
+
+export default class Card {
+    constructor(data, selector) {
+        this._image = data.link;
+        this._title = data.name;
+        this._owner = data.owner._id;
+        this._cardId = data._id;
+        this._likes = data.likes;
+    };
+
+    _getElement() {
+        const cardElement = document
+            .querySelector('#gallery__item')
+            .content
+            .querySelector('.gallary__item')
+            .cloneNode(true);
+        return cardElement;
+    };
+
+    _handleLikeClick() {
+        this._element.querySelector('.card__like-btn').classList.add('card__like-btn_active');
+    };
+
+    _handleImageClick() {
+        console.log('Open Image Popup')
+            //There should be a function to open image popup
+    };
+
+    _handleTrashClick() {
+        console.log('Delete card');
+        //There should be a function to delete image card
+    };
+
+    _setEventListeners() {
+        this._element.querySelector('.card__like-btn').addEventListener('click', () => {
+            this._handleLikeClick()
+        });
+        this._element.querySelector('.card__image').addEventListener('click', () => {
+            this._handleImageClick();
+        });
+        this._element.querySelector('.card__trash-btn').addEventListener('click', () => {
+            this._handleTrashClick();
+        });
+    };
+
+
+
+    generate() {
+        this._element = this._getElement();
+        this._setEventListeners();
+        this._element.querySelector('.card__image').src = this._image;
+        this._element.querySelector('.card__title').textContent = this._title;
+        this._element.querySelector('.card__like-counter').textContent = this._likes.length;
+    }
+
+
+}
+
 
 
 //показать лайки пользователя
-function containsClientLikes (likes, clientId) {
-  const result = likes.some((like) => {
-    return like._id === clientId;
-  })
-  return result
+function containsClientLikes(likes, clientId) {
+    const result = likes.some((like) => {
+        return like._id === clientId;
+    })
+    return result
 }
 
-function renderLikes (counter, button, likes, clientId) {
-  if (containsClientLikes(likes, clientId)) {
-    button.classList.add('card__like-btn_active')
-  } else {
-    button.classList.remove('card__like-btn_active')
-  }
-  counter.textContent = likes.length;
+function renderLikes(counter, button, likes, clientId) {
+    if (containsClientLikes(likes, clientId)) {
+        button.classList.add('card__like-btn_active')
+    } else {
+        button.classList.remove('card__like-btn_active')
+    }
+    counter.textContent = likes.length;
 }
 
 
 
 //создать карточку из шаблона
-function createCard (link, name, likes, ownerId, clientId, cardId) {
-  const galleryItem = galleryItemTemplate.querySelector('.gallery__item').cloneNode(true);
-  const likeButton = galleryItem.querySelector('.card__like-btn');
-  const deleteButton = galleryItem.querySelector('.card__trash-btn');
-  const cardPhoto = galleryItem.querySelector('.card__image');
-  const cardTitle = galleryItem.querySelector('.card__title');
-  const likesCounter = galleryItem.querySelector('.card__like-counter');
+function createCard(link, name, likes, ownerId, clientId, cardId) {
+    const galleryItem = galleryItemTemplate.querySelector('.gallery__item').cloneNode(true);
+    const likeButton = galleryItem.querySelector('.card__like-btn');
+    const deleteButton = galleryItem.querySelector('.card__trash-btn');
+    const cardPhoto = galleryItem.querySelector('.card__image');
+    const cardTitle = galleryItem.querySelector('.card__title');
+    const likesCounter = galleryItem.querySelector('.card__like-counter');
 
-  cardPhoto.src = link;
-  cardPhoto.alt = name;
-  cardTitle.textContent = name;
-  galleryItem.dataset.id = cardId;
+    cardPhoto.src = link;
+    cardPhoto.alt = name;
+    cardTitle.textContent = name;
+    galleryItem.dataset.id = cardId;
 
-  if (ownerId === clientId) {
-    deleteButton.addEventListener('click', (evt) => {
-      removebleCard.element = evt.currentTarget.closest('.gallery__item')
-      removebleCard.cardId = removebleCard.element.dataset.id
-      openPopup(popupDeleteCard)
-    });
-  } else {
-    deleteButton.remove()
-  }
-
-  //«лайк»
-  renderLikes(likesCounter, likeButton, likes, clientId);
-
-  likeButton.addEventListener('click', (evt) => {
-    const cardId = evt.currentTarget.closest('.gallery__item').dataset.id;
-
-    if (!evt.target.classList.contains('card__like-btn_active')) {
-      putLike(cardId)
-      .then((res) => {
-        renderLikes(likesCounter, likeButton, res.likes, clientId);
-      })
-      .catch((e) => {
-        console.log(e);
-      })
+    if (ownerId === clientId) {
+        deleteButton.addEventListener('click', (evt) => {
+            removebleCard.element = evt.currentTarget.closest('.gallery__item')
+            removebleCard.cardId = removebleCard.element.dataset.id
+            openPopup(popupDeleteCard)
+        });
     } else {
-      deleteLike(cardId)
-      .then((res) => {
-        renderLikes(likesCounter, likeButton, res.likes, clientId);
-      })
-      .catch((e) => {
-        console.log(e);
-      })
+        deleteButton.remove()
     }
-  });
 
-  //«просмотр фото»
-  cardPhoto.addEventListener('click', (evt) => {
-    const eventTarget = evt.target;
-    const photoSrc = eventTarget.src;
-    const photoTitle = eventTarget.alt;
-    viewPhoto (photoSrc, photoTitle, photoTitle);
-  });
-  
+    //«лайк»
+    renderLikes(likesCounter, likeButton, likes, clientId);
 
-  return galleryItem;
+    likeButton.addEventListener('click', (evt) => {
+        const cardId = evt.currentTarget.closest('.gallery__item').dataset.id;
+
+        if (!evt.target.classList.contains('card__like-btn_active')) {
+            putLike(cardId)
+                .then((res) => {
+                    renderLikes(likesCounter, likeButton, res.likes, clientId);
+                })
+                .catch((e) => {
+                    console.log(e);
+                })
+        } else {
+            deleteLike(cardId)
+                .then((res) => {
+                    renderLikes(likesCounter, likeButton, res.likes, clientId);
+                })
+                .catch((e) => {
+                    console.log(e);
+                })
+        }
+    });
+
+    //«просмотр фото»
+    cardPhoto.addEventListener('click', (evt) => {
+        const eventTarget = evt.target;
+        const photoSrc = eventTarget.src;
+        const photoTitle = eventTarget.alt;
+        viewPhoto(photoSrc, photoTitle, photoTitle);
+    });
+
+
+    return galleryItem;
 }
 
 
 //-----просмотр фотографии карточки
-function viewPhoto (src, alt, title) {
-  photo.src = src;
-  photo.alt = alt;
-  caption.textContent = title;
-  openPopup(popupViewPhoto)
+function viewPhoto(src, alt, title) {
+    photo.src = src;
+    photo.alt = alt;
+    caption.textContent = title;
+    openPopup(popupViewPhoto)
 }
 
 
--- src/components/constants.js
+++ src/components/constants.js
@@ -1,7 +1,12 @@
 export const API_OPTIONS = {
-  baseUrl: 'https://nomoreparties.co/v1/plus-cohort-9',
-  headers: {
-    authorization: 'b5011ed4-a731-4745-a185-1e316e913ed1',
-    'Content-Type': 'application/json'
-  }
+    baseUrl: 'https://nomoreparties.co/v1/plus-cohort-9',
+    headers: {
+        authorization: 'b5011ed4-a731-4745-a185-1e316e913ed1',
+        'Content-Type': 'application/json'
+    }
 }
+
+export const galleryItemTemplate = document.querySelector('#gallery__item').content;
+export const popupViewPhoto = document.querySelector('.popup_feature_photo');
+export const photo = document.querySelector('.view-photo__image');
+export const caption = document.querySelector('.view-photo__caption');
